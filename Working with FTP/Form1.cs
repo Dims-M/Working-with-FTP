ff4a1be7a70fc0822c0de6d5ad052878c5fbba94
Дмитрий <rabolan@mail.ru>
2019-05-19 13:52:56 +0300

    ntyb

--- Working with FTP/Form1.cs
+++ Working with FTP/Form1.cs
@@ -1,4 +1,5 @@
-﻿using MaterialSkin.Controls;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,6 +17,12 @@ namespace Working_with_FTP
         public Form1()
         {
             InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT; // тема. 2 шт
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey500, Primary.BlueGrey900,Primary.BlueGrey500,Accent.LightBlue200,TextShade.WHITE);
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
