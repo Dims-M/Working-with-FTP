e345052dd195bdc9049489f5fe57ee4f57b24697
Дмитрий <rabolan@mail.ru>
2019-05-19 21:19:32 +0300

    ебля с папками

--- Working with FTP/Form1.cs
+++ Working with FTP/Form1.cs
@@ -76,37 +76,45 @@ namespace Working_with_FTP
             {
                 if (Directory.Exists(item)) // проверяем. Кинули нам папку или отдельный файл
                 {
-                     materialLabel1.Text += "Является директорией, брошенной папкой )))";
+                    // materialLabel1.Text += "Является директорией, брошенной папкой )))\t\n";
 
                     myList.AddRange(Directory.GetFiles(item, "*.*",SearchOption.AllDirectories)); // добавляем в лист пути к файлам из папки.Будуд добавленны все файлы по маске bp dct gjlrfnjkjujd 
-                }
-
-                if ((Directory.Exists(item))!= true) // проверяем. Кинули нам папку или отдельный файл
-                {
-                    materialLabel1.Text += "Является директорией, брошенной папкой )))";
-
-                    myList.Add(item); // добавляем в лист пути к файлам из папки.Будуд добавленны все файлы по маске bp dct gjlrfnjkjujd 
-                }
-
-                //Вывод в лог
-                else if (count != 3 ) 
-                {
-                    // materialLabel1.Text += "Показаны только 3 первых файла";
-                    //break;
-                    materialLabel1.Text += item;
-                    count++;
                     materialLabel1.Text += "\t\n";
                 }
-                   
-                else
-                {
-                    //materialLabel1.Text += item;
-                    //materialLabel1.Text += "\t\n";
 
-                    materialLabel1.Text += "Показаны только 3 первых файла";
-                    break;
+              else  if ((Directory.Exists(item))!= true) // проверяем. Кинули нам папку или отдельный файл
+                {
+                   // materialLabel1.Text += "Является файлом  )))\t\n";
+
+                    myList.Add(item); //  добавление в лист
+
+                 //   materialLabel1.Text += string.Join("\t\n",myList); // конвертация листа в стринг и вывод 
+                    materialLabel1.Text += "\t\n";
                 }
-        } 
+
+                materialLabel1.Text += string.Join("\t\n", myList); // конвертация листа в стринг и вывод 
+                #region Старые методы
+                ////Вывод в лог
+                //else if (count != 3 ) 
+                //{
+                //    // materialLabel1.Text += "Показаны только 3 первых файла";
+                //    //break;
+                //    materialLabel1.Text += item;
+                //    count++;
+                //    materialLabel1.Text += "\t\n";
+                //}
+
+                //else
+                //{
+                //    //materialLabel1.Text += item;
+                //    //materialLabel1.Text += "\t\n";
+
+                //    materialLabel1.Text += "Показаны только 3 первых файла";
+                //    break;
+                //}
+
+                #endregion
+            }
         }
 
         
